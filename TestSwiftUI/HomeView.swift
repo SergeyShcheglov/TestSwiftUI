fcdd9ec1857750205894ed770797602378ec3a2d
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-25 21:22:43 +0600

    Add UI

--- TestSwiftUI/HomeView.swift
+++ TestSwiftUI/HomeView.swift
@@ -8,8 +8,93 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var backgroundImage = Image("horizontal-background")
+    @State private var profileImage = Image("mcconaughey")
+    
+    @State private var name = "John Doe"
+    @State private var bio = "This is bio"
+    
+    @State private var postText = "If you're looking for a cute dessert for a party or even just a little pick-me up, try making some of @bakedbyjosie's bite sized cheesecakes! For more delicious baked goods, watch Baked by Josie"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                backgroundImage
+                    .resizable()
+                    .scaledToFit()
+                
+                profileImage
+                    .resizable()
+                    .clipShape(Circle())
+                    .scaledToFill()
+                    .frame(width: 180, height: 180, alignment: .center)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 15))
+                    .offset(y: -90)
+                    .padding(.bottom, -90)
+            }
+            
+            
+            VStack(alignment: .center) {
+                Text(name)
+                    .font(.title)
+                    .frame(maxWidth: .infinity)
+                Text(bio)
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+            }
+            
+            
+            Divider()
+            
+            Text("Posts")
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            VStack {
+                HStack {
+                    profileImage
+                        .resizable()
+                        .clipShape(Circle())
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .leading)
+                    
+                    VStack {
+                        Text(name)
+                        
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                
+                Text(postText)
+                
+                profileImage
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+            
+            VStack {
+                HStack {
+                    profileImage
+                        .resizable()
+                        .clipShape(Circle())
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .leading)
+                    
+                    Text(name)
+                    Spacer()
+                }
+                
+                Text(postText)
+                
+                profileImage
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+        }
     }
 }
 
