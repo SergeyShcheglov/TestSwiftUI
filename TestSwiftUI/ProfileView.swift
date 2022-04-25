6bafdec35160e1045a48af2fc963d74561e31946
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-25 21:22:51 +0600

    Add UI

--- TestSwiftUI/ProfileView.swift
+++ TestSwiftUI/ProfileView.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var backgroundImage = Image("horizontal-background")
+    @State private var profileImage = Image("mcconaughey")
+    @State private var sampleText = "Describe yourself..."
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Profile picture")
+                    
+                    Spacer()
+                    
+                    Button("Edit") {
+                        
+                    }
+                }
+                
+                ZStack {
+                    profileImage
+                        .resizable()
+//                        .scaledToFit()
+                        .clipShape(Circle())
+                        .scaledToFill()
+                        .frame(width: 150, height: 150, alignment: .center)
+                }
+                
+                
+            }
+            
+            Divider()
+            
+            VStack {
+                HStack {
+                    Text("Cover photo")
+                    
+                    Spacer()
+                    
+                    Button("Edit") {
+                        
+                    }
+                }
+                backgroundImage
+                    .resizable()
+                    .scaledToFit()
+                    
+                
+            }
+            
+            Divider()
+            
+            VStack {
+                HStack {
+                    Text("Bio")
+                    Spacer()
+                    
+                    Button("Edit") {
+                        
+                    }
+                }
+                
+                TextField("Add text to your bio", text: $sampleText)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+//                    .frame(maxWidth: .infinity, alignment: .center)
+                
+                Divider()
+            }
+        }
+        .padding()
     }
 }
 
