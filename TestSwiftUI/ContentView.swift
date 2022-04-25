f2473a98cb69a277923bf98c8a9c63a48aa7df77
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-25 16:25:50 +0600

    Add tab items

--- TestSwiftUI/ContentView.swift
+++ TestSwiftUI/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("", systemImage: <#T##String#>)
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("", systemImage: <#T##String#>)
+                }
+        }
     }
 }
 
