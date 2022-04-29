15bf9f016e3d92ee6d148ea3e4d8754b133e8e2c
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-29 18:27:46 +0600

    clean a little

---
+++ TestSwiftUI/Application/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Sergey Shcheglov on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
